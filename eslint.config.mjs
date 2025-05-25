989a674620250eaac5526f41c554d55aea181f53
Abdul Moeez Shaikh <abdulmoeezshaikh@gmail.com>
2025-05-25 19:45:39 -0400

    another fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-var-requires": "off",
       "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
     },
   },
 ];
